a26f2674b1edf6d12362e768948c3592e8a9495c
tuffzinc <judethegmr17@gmail.com>
2026-06-15 00:23:00 -0700

    Update and rename digbick.js to warning.js

--- data/js/digbick.js
+++
@@ -1,3 +0,0 @@
-// this is the best js file ever
-
-console.log("your in the fucking leaderboard now dont paste shitty code here that some cunt said would give you free elo")
---
+++ data/js/warning.js
@@ -0,0 +1,7 @@
+// this is the best js file ever
+
+console.log(
+    "%cHEY! %cWELCOME TO MEDOTYPE and remember dont paste shitty code here that some cunt said would give you free elo",
+    "color: #ff3333; font-size: 50px; font-weight: 800; font-family: sans-serif; text-shadow: 3px 3px 0px #000; padding-bottom: 10px;",
+    "color: #ffcc00; font-size: 16px; font-weight: bold; font-family: sans-serif; background-color: #111; padding: 10px; border-left: 5px solid #ff3333; border-radius: 4px;"
+);
