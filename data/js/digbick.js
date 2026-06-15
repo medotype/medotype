7d18260a68886d3c6311faa9f374d3b8e8c0e127
tuffzinc <judethegmr17@gmail.com>
2026-06-15 00:20:41 -0700

    Add comments and update console log message

--- data/js/digbick.js
+++ data/js/digbick.js
@@ -1,1 +1,3 @@
-console.log("dick bick boiiiiii")
+// this is the best js file ever
+
+console.log("your in the fucking leaderboard now dont paste shitty code here that some cunt said would give you free elo")
